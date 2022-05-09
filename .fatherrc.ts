696dc659b7fe75ce75fc274a54eb64e3c04b1057
heiyexing <496845051@qq.com>
2022-05-09 16:36:43 +0800

    feat: 1.打包输出新增commonjs和umd版本

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,13 +1,18 @@
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
+import analyze from 'rollup-plugin-analyzer';
+
 export default {
   esm: 'babel',
   cjs: 'babel',
   umd: {
     name: 'L7.Draw',
     file: 'l7-draw',
+    minFile: true,
     sourcemap: true,
     globals: {
       '@antv/l7': 'L7',
     },
   },
+  extraRollupPlugins: [optimizeLodashImports(), analyze()],
   lessInBabelMode: true,
 };
