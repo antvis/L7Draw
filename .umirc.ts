594104fd6750cbfe9894fb4437be171b436290cd
yanxiong <oujinhui.ojh@antgroup.com>
2022-06-22 17:27:42 +0800

    fix: 新增.husky文件

--- .umirc.ts
+++ .umirc.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   mode: 'site',
   extraBabelIncludes: ['@antv/dumi-theme-antv'],
-
   navs: [
     {
       title: '文档',
