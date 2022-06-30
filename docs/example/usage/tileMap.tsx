6a58954249a098d669139922d0a312af647e9042
yanxiong <oujinhui.ojh@antgroup.com>
2022-06-30 14:07:00 +0800

    docs: 1.新增遥感绘制案例

--- docs/example/usage/tileMap.tsx
+++ docs/example/usage/tileMap.tsx
@@ -1,12 +1,13 @@
-import { Scene } from '@antv/l7';
-import { DrawRect } from '@antv/l7-draw';
+import { PointLayer, Scene } from '@antv/l7';
+import { DrawEvent, DrawPolygon } from '@antv/l7-draw';
 import { GaodeMapV2 } from '@antv/l7-maps';
+import { bbox, pointsWithinPolygon, randomPoint } from '@turf/turf';
 import React, { useEffect, useState } from 'react';
 
 const id = String(Math.random());
 
 const Demo: React.FC = () => {
-  const [rectDrawer, setRectDrawer] = useState<DrawRect | null>(null);
+  const [polygonDrawer, setPolygonDrawer] = useState<DrawPolygon | null>(null);
 
   useEffect(() => {
     const scene = new Scene({
@@ -19,23 +20,56 @@ const Demo: React.FC = () => {
       }),
     });
     scene.on('loaded', () => {
-      const drawer = new DrawRect(scene, {
+      scene.addImage(
+        'site',
+        'https://gw.alipayobjects.com/mdn/rms_2591f5/afts/img/A*i7uyS6lLupYAAAAAAAAAAAAAARQnAQ',
+      );
+      const drawer = new DrawPolygon(scene, {
+        editable: false,
         style: {
           polygon: {
             normal: {
-              color: 'rbga(0,0,0,0)',
+              color: 'rgba(0,0,0,0)',
+            },
+            hover: {
+              color: 'rgba(0,0,0,0)',
+            },
+            active: {
+              color: 'rgba(0,0,0,0)',
             },
           },
         },
       });
-      setRectDrawer(drawer);
+      const layer = new PointLayer({
+        blend: 'normal',
+      })
+        .source([])
+        .shape('site')
+        .size(12)
+        .style({
+          offsets: [0, 16],
+        });
+      scene.addLayer(layer);
+      setPolygonDrawer(drawer);
       drawer.enable();
+      (scene.map as any).addLayer(new window.AMap.TileLayer.Satellite());
+
+      drawer.on(DrawEvent.Add, (newPolygon) => {
+        const box = bbox(newPolygon);
+        const data = pointsWithinPolygon(
+          randomPoint(80, {
+            bbox: box,
+          }),
+          newPolygon,
+        );
+        layer.setData(data);
+      });
     });
   }, []);
 
   return (
     <div>
-      <div id={id} style={{ height: 400, position: 'relative' }} />
+      <div id={id} style={{ height: 600, position: 'relative' }} />
     </div>
   );
 };
