dd876a77cf30c9c112001cf76e6f227d0bd8b6e1
heiyexing <496845051@qq.com>
2023-04-20 16:48:23 +0800

    fix: 新增获取单一颜色 style 配置的工具方法 (#124)
    Co-authored-by: yanxiong <oujinhui.ojh@antgroup.com>

---
+++ src/utils/style.ts
@@ -0,0 +1,41 @@
+import { DeepPartial, IStyle } from '../typings';
+
+/**
+ * 获取单个颜色的 style 供 draw 实例使用
+ * @param color 
+ * @returns 
+ */
+export function getSingleColorStyle(color: string): DeepPartial<IStyle> {
+  return {
+    point: {
+      normal: { color },
+      hover: { color },
+      active: { color },
+    },
+    line: {
+      normal: { color },
+      hover: { color },
+      active: { color },
+    },
+    polygon: {
+      normal: { color },
+      hover: { color },
+      active: { color },
+
+      style: {
+        // @ts-ignore
+        opacity: 0.2,
+      },
+    },
+    text: {
+      normal: { color },
+      active: { color },
+    },
+    midPoint: {
+      normal: { color },
+    },
+    dashLine: {
+      normal: { color },
+    },
+  };
+}
